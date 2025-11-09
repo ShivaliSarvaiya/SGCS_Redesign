2b4cb836b1a6896fbba77d9f34a7337bec5f7c86
ShivaliSarvaiya <ssarvaiya@eastsideprep.org>
2025-11-08 22:18:03 -0800

    homepage stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+let index = 0;
+const slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    index++;
+    if (index > slides.length) {
+        index = 1;
+    }
+    slides[index - 1].style.display = "block";
+    setTimeout(showSlides, 3000);
+}
+
+showSlides();
